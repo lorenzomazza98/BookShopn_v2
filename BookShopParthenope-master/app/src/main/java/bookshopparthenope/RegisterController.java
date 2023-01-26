09d40531d8de2c0155d436b45cb169e6ab457fd0
Enrico39 <enrico.madonna001@studenti.uniparthenope.it>
2023-01-26 23:53:53 +0100

    back to login button

---
+++ BookShopParthenope-master/app/src/main/java/bookshopparthenope/RegisterController.java
@@ -0,0 +1,65 @@
+package bookshopparthenope;
+
+
+
+import bookshopparthenope.Model.UserManagement.Customer;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RegisterController  implements Initializable {
+
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Button registerButton;
+
+    @FXML
+    private TextField registerCognome;
+
+    @FXML
+    private TextField registerEmail;
+
+    @FXML
+    private TextField registerNome;
+
+    @FXML
+    private PasswordField registerPassword;
+
+    @FXML
+    private TextField registerUsername;
+
+    @FXML
+    void registerUser(ActionEvent event) {
+
+    }
+
+    @FXML
+    void switchToLogin(ActionEvent event) throws IOException {
+        root= FXMLLoader.load(getClass().getResource("/bookshopparthenope/gui/login.fxml"));
+        Stage window = (Stage) loginButton.getScene().getWindow();
+        window.setScene(new Scene(root,650,450));
+    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
